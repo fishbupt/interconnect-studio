2e85a91b6596348c6cf518f5ba454727793d7cbf
fishbupt <fishbupt@gmail.com>
2026-09-22 16:27:26 +0800

    算法：导出 S 参数 Trace 构建接口

--- src/interconnect_studio/algorithms/network/__init__.py
+++ src/interconnect_studio/algorithms/network/__init__.py
@@ -2,5 +2,15 @@
 
 from interconnect_studio.algorithms.network.formats import SParameterFormat, format_s_parameter
 from interconnect_studio.algorithms.network.port_mapping import remap_ports
+from interconnect_studio.algorithms.network.traces import (
+    create_s_parameter_trace,
+    plot_kind_for_s_parameter_format,
+)
 
-__all__ = ["SParameterFormat", "format_s_parameter", "remap_ports"]
+__all__ = [
+    "SParameterFormat",
+    "create_s_parameter_trace",
+    "format_s_parameter",
+    "plot_kind_for_s_parameter_format",
+    "remap_ports",
+]
