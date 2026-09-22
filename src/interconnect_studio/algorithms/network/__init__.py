174df17d82f9d7eb428eefbba62068c94feeeb2d
fishbupt <fishbupt@gmail.com>
2026-09-22 15:58:06 +0800

    算法：导出 S 参数显示格式接口

--- src/interconnect_studio/algorithms/network/__init__.py
+++ src/interconnect_studio/algorithms/network/__init__.py
@@ -1,5 +1,6 @@
 """Network transformation algorithms."""
 
+from interconnect_studio.algorithms.network.formats import SParameterFormat, format_s_parameter
 from interconnect_studio.algorithms.network.port_mapping import remap_ports
 
-__all__ = ["remap_ports"]
+__all__ = ["SParameterFormat", "format_s_parameter", "remap_ports"]
