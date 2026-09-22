86de8cf8bf33e5218449ee758f128e3a476701b0
fishbupt <fishbupt@gmail.com>
2026-09-22 16:26:46 +0800

    领域模型：导出 Trace 与 Plot Model

--- src/interconnect_studio/core/__init__.py
+++ src/interconnect_studio/core/__init__.py
@@ -2,5 +2,7 @@
 
 from interconnect_studio.core.errors import DataFormatError, InputValidationError
 from interconnect_studio.core.network import Network
+from interconnect_studio.core.plot import PlotKind, PlotModel
+from interconnect_studio.core.trace import Trace
 
-__all__ = ["DataFormatError", "InputValidationError", "Network"]
+__all__ = ["DataFormatError", "InputValidationError", "Network", "PlotKind", "PlotModel", "Trace"]
