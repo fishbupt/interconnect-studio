fdb49572b5e8391981bc3d3b249bc9c828225173
fishbupt <fishbupt@gmail.com>
2026-09-22 15:31:45 +0800

    领域模型：添加数据格式异常

--- src/interconnect_studio/core/errors.py
+++ src/interconnect_studio/core/errors.py
@@ -3,3 +3,7 @@
 
 class InputValidationError(ValueError):
     """Raised when input data violates a core domain invariant."""
+
+
+class DataFormatError(ValueError):
+    """Raised when an input file or serialized representation is invalid."""
