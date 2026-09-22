2956b815a79e3df10ca3c174126eb744a8fe4de7
fishbupt <fishbupt@gmail.com>
2026-09-22 15:42:29 +0800

    输入输出：导出 Touchstone Writer

--- src/interconnect_studio/io/__init__.py
+++ src/interconnect_studio/io/__init__.py
@@ -1,5 +1,5 @@
 """Input/output support for Interconnect Studio."""
 
-from interconnect_studio.io.touchstone import read_touchstone
+from interconnect_studio.io.touchstone import read_touchstone, write_touchstone
 
-__all__ = ["read_touchstone"]
+__all__ = ["read_touchstone", "write_touchstone"]
